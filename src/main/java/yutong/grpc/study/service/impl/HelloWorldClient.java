70e7fdd987fac69d51edf08bcfaa001e04406b75
joker <yutong467942250@hotmail.com>
2017-07-19 09:45:29 +0800

    hello 余童 grpc

---
+++ src/main/java/yutong/grpc/study/service/impl/HelloWorldClient.java
@@ -0,0 +1,65 @@
+package yutong.grpc.study.service.impl;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author yuton
+ * @version 1.0
+ * @description
+ * @since 2017/7/19 9:39
+ */
+public class HelloWorldClient {
+    private final ManagedChannel channel;
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    /** Construct client connecting to HelloWorld server at {@code host:port}. */
+    public HelloWorldClient(String host, int port) {
+        channel = ManagedChannelBuilder.forAddress(host, port)
+                .usePlaintext(true)
+                .build();
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    /** Say hello to server. */
+    public void greet(String name) {
+        System.out.println("Will try to greet " + name + " ...");
+        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+        HelloReply response;
+        try {
+            response = blockingStub.sayHello(request);
+        } catch (StatusRuntimeException e) {
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("Greeting: " + response.getMessage());
+    }
+
+    /**
+     * Greet server. If provided, the first element of {@code args} is the name to use in the
+     * greeting.
+     */
+    public static void main(String[] args) throws Exception {
+        HelloWorldClient client = new HelloWorldClient("localhost", 50051);
+        try {
+      /* Access a service running on the local machine on port 50051 */
+            String user = "余童";
+            if (args.length > 0) {
+                user = args[0]; /* Use the arg as the name to greet if provided */
+            }
+            client.greet(user);
+        } finally {
+            client.shutdown();
+        }
+    }
+}
---
+++ src/main/java/yutong/grpc/study/service/impl/HelloWorldServer.java
@@ -0,0 +1,70 @@
+package yutong.grpc.study.service.impl;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+
+/**
+ * @author yuton
+ * @version 1.0
+ * @description
+ * @since 2017/7/18 18:09
+ */
+public class HelloWorldServer {
+    private int port = 50051;
+    private Server server;
+
+    private void start() throws IOException {
+        server = ServerBuilder.forPort(port)
+                .addService(new GreeterImpl())
+                .build()
+                .start();
+       System.out.println("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+            System.err.println("*** shutting down gRPC server since JVM is shutting down");
+            HelloWorldServer.this.stop();
+            System.err.println("*** server shut down");
+        }));
+    }
+
+    private void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    /**
+     * Main launches the server from the command line.
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final HelloWorldServer server = new HelloWorldServer();
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+    private class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+
+        @Override
+        public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+            System.out.println("======sayHello========");
+            HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+    }
+}
